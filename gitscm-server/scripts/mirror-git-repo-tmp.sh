6c675b82577e81d4ae43b72abda312f1b6f4b0bb
nishit <nishit.goel@redblink.net>
2022-03-02 14:08:03 +0530

    removed option for code and manifest

--- gitscm-server/scripts/mirror-git-repo-tmp.sh
+++ gitscm-server/scripts/mirror-git-repo-tmp.sh
@@ -1,5 +1,5 @@
 cd /home/git/repos
 ls="$(ls | awk 'FNR==1{print $1}')"
-git clone --bare https://gitlab.redblink.net/PureStorage/java-app-manifest.git
-cd java-app-manifest.git
+git clone --bare https://github.com/PureStorage-OpenConnect/javaApplication-code.git
+cd javaApplication-code.git
 git push --mirror /home/git/repos/$ls
