6c3a67d3703ea1d978c75c0af88a82ffa2497da1
nishit93-hub <64061293+nishit93-hub@users.noreply.github.com>
2022-03-30 16:53:51 +0530

    Update install-ArgoEvents.sh

--- ci-cd-workflow/scripts/install-ArgoEvents.sh
+++ ci-cd-workflow/scripts/install-ArgoEvents.sh
@@ -1,5 +1,5 @@
 kubectl create ns argo-events &&
-sleep 1
+sleep 5
 kubectl apply  -n argo-events -f https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/install.yaml &&
-sleep 1
+sleep 5
 kubectl apply -n argo-events -f https://raw.githubusercontent.com/argoproj/argo-events/stable/examples/eventbus/native.yaml
