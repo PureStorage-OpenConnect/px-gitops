ba978d130e6cef4f7f7bbcecadd7ebfa9ea94613
root <root@sn1-r720-g09-31.puretec.purestorage.com>
2022-03-30 07:27:05 -0700

    update submit-wordpress-workflow-template.sh

---
+++ ci-cd-workflow/argo-worflow/wordpress-app/px-snaphshot-restore-script-template/create-snapshot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e -u
+set -o pipefail
+apt-get update 
+apt-get install -y apt-transport-https ca-certificates curl        
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubectl
+cp /snapshot-script/px-snapshot.yaml /tmp/px-snapshot-tmp.yaml
+Noresource="No resources found in XX-namespace-XX namespace."
+getSnapshot="$(kubectl get VolumeSnapshot -n XX-namespace-XX 2>&1)"
+if [[ "$Noresource" == "$getSnapshot" ]]; then
+  sed -i "s,XX-namespace-XX,1,g" /tmp/px-snapshot-tmp.yaml
+  kubectl apply -f /tmp/px-snapshot-tmp.yaml
+else
+  vSNAPSHOT="$(kubectl get VolumeSnapshot -n XX-namespace-XX -l=name=snapshot-of-wordpress-app --sort-by=.metadata.creationTimestamp -o jsonpath='{.items[-1:].metadata.name}')"
+  snapshotID="$(echo $vSNAPSHOT | cut -d"-" -f4)"
+  echo $snapshotID;
+  NewID="$(expr $snapshotID + 1)"
+  echo $NewID
+  sed -i "s,XX-namespace-XX,$NewID,g" /tmp/px-snapshot-tmp.yaml
+  kubectl apply -f /tmp/px-snapshot-tmp.yaml
+fi
+sleep 5
+kubectl get VolumeSnapshot -n XX-namespace-XX
