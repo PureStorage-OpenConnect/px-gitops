3a3dfffc9faa961e4c47ebd754520500788a30c4
root <root@sn1-r720-g09-31.puretec.purestorage.com>
2022-03-30 07:27:05 -0700

    update submit-wordpress-workflow-template.sh

---
+++ ci-cd-workflow/argo-worflow/wordpress-app/px-snaphshot-restore-script-template/restore-snapshot.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e -u
+set -o pipefail
+apt-get update 
+apt-get install -y apt-transport-https ca-certificates curl        
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubectl
+cp /restore-snapshot-script/px-restore-snapshot.yaml /tmp/px-restore-snapshot-tmp.yaml
+Noresource="No resources found in XX-namespace-XX namespace."
+getSnapshotRestore="$(kubectl get VolumeSnapshotRestore -n XX-namespace-XX 2>&1)"
+if [[ "$Noresource" == "$getSnapshotRestore" ]]; then
+    sed -i "s,XX-restorename-XX,1,g" /tmp/px-restore-snapshot-tmp.yaml
+    vSNAPSHOT="$(kubectl get VolumeSnapshot -n XX-namespace-XX -l=name=snapshot-of-wordpress-app --sort-by=.metadata.creationTimestamp -o jsonpath='{.items[-1:].metadata.name}')"
+    echo $vSNAPSHOT
+    sed -i "s,XX-snapshot-name-XX,$vSNAPSHOT,g" /tmp/px-restore-snapshot-tmp.yaml
+    cat /tmp/px-restore-snapshot-tmp.yaml
+    kubectl apply -f /tmp/px-restore-snapshot-tmp.yaml
+    sleep 5
+    kubectl describe -f /tmp/px-restore-snapshot-tmp.yaml
+else
+    vSnapshotRestore="$(kubectl get VolumeSnapshotRestore -n XX-namespace-XX -l=name=restore-of-wordpress-app  --sort-by=.metadata.creationTimestamp -o jsonpath='{.items[-1:].metadata.name}')"
+    SnapshotRestoreID="$(echo $vSnapshotRestore | cut -d"-" -f4)"
+    echo $SnapshotRestoreID;
+    NewID="$(expr $SnapshotRestoreID + 1)"
+    echo $NewID
+    sed -i "s,XX-restorename-XX,$NewID,g" /tmp/px-restore-snapshot-tmp.yaml
+    vSNAPSHOT="$(kubectl get VolumeSnapshot -n XX-namespace-XX -l=name=snapshot-of-wordpress-app --sort-by=.metadata.creationTimestamp -o jsonpath='{.items[-1:].metadata.name}')"
+    echo $vSNAPSHOT
+    sed -i "s,XX-snapshot-name-XX,$vSNAPSHOT,g" /tmp/px-restore-snapshot-tmp.yaml
+    cat /tmp/px-restore-snapshot-tmp.yaml
+    kubectl apply -f /tmp/px-restore-snapshot-tmp.yaml
+    sleep 5
+    kubectl describe -f /tmp/px-restore-snapshot-tmp.yaml
+    kubectl get VolumeSnapshotRestore -n wordpress-main
+fi        
---
+++ ci-cd-workflow/scripts/submit-wordpress-workflow-template.sh
@@ -0,0 +1,68 @@
+echo > ../argo-worflow/wordpress-app/px-snaphost-restore-script/px-restore-snapshot.yaml
+echo > ../argo-worflow/wordpress-app/px-snaphost-restore-script/create-snapshot.sh
+echo > ../argo-worflow/wordpress-app/px-snaphost-restore-script/restore-snapshot.sh
+echo > ../argo-worflow/wordpress-app/px-snaphost-restore-script/px-snapshot.yaml
+
+cp ../argo-worflow/wordpress-app/px-snaphshot-restore-script-template/px-restore-snapshot.yaml ../argo-worflow/wordpress-app/px-snaphost-restore-script/px-restore-snapshot.yaml
+cp ../argo-worflow/wordpress-app/px-snaphshot-restore-script-template/create-snapshot.sh   ../argo-worflow/wordpress-app/px-snaphost-restore-script/create-snapshot.sh
+cp ../argo-worflow/wordpress-app/px-snaphshot-restore-script-template/restore-snapshot.sh  ../argo-worflow/wordpress-app/px-snaphost-restore-script/restore-snapshot.sh
+cp ../argo-worflow/wordpress-app/px-snaphshot-restore-script-template/px-snapshot.yaml  ../argo-worflow/wordpress-app/px-snaphost-restore-script/px-snapshot.yaml
+
+
+
+CurrentClusterPath="$(echo $KUBECONFIG)"
+echo $CurrentClusterPath
+echo "Creating kubernetes secret for jfrog credentials in argo namespace"
+kubectl create secret generic jfrog-config -n argo \
+    --from-file=.dockerconfigjson=../config.json \
+    --type=kubernetes.io/dockerconfigjson
+sleep 2
+echo "                                                    "           
+source ../setup-vars/setup-vars
+echo "                                                    "
+sed -ie "s,XX-namespace-XX,$gitRepoNamespace,g" ../argo-worflow/wordpress-app/px-snaphost-restore-script/px-restore-snapshot.yaml
+sed -ie "s,XX-repo-XX,$gitRepoName,g" ../argo-worflow/wordpress-app/px-snaphost-restore-script/px-snapshot.yaml
+sed -ie "s,XX-namespace-XX,$gitRepoNamespace,g"  ../argo-worflow/wordpress-app/px-snaphost-restore-script/create-snapshot.sh
+sed -ie "s,XX-namespace-XX,$gitRepoNamespace,g" ../argo-worflow/wordpress-app/px-snaphost-restore-script/restore-snapshot.sh
+sed -ie "s,XX-namespace-XX,$gitRepoNamespace,g" ../argo-worflow/wordpress-app/px-snaphost-restore-script/px-snapshot.yaml
+sleep 2
+
+    countYaml=`ls -1 ../argo-worflow/wordpress-app/px-snaphost-restore-script/*.yamle 2>/dev/null | wc -l`
+    countScripts=`ls -1 ../argo-worflow/wordpress-app/px-snaphost-restore-script/*.she 2>/dev/null | wc -l`
+    if [ "$countYaml" != "0" ] && [ "$countScripts" != "0" ]
+    then 
+    rm ../argo-worflow/wordpress-app/px-snaphost-restore-script/*.yamle
+    rm ../argo-worflow/wordpress-app/px-snaphost-restore-script/*.she
+    fi
+    sleep 2
+
+
+echo "Creating kubernetes secret for px snapshot script "
+kubectl create secret generic wordpress-snapshot-script --from-file=../argo-worflow/wordpress-app/px-snaphost-restore-script/px-snapshot.yaml  --from-file=../argo-worflow/wordpress-app/px-snaphost-restore-script/create-snapshot.sh -n argo
+sleep 2
+echo "                                                    "
+echo "                                                    "
+echo "Creating kubernetes secret for px restore snapshot script "
+kubectl create secret generic wordpress-restore-snapshot-script  --from-file=../argo-worflow/wordpress-app/px-snaphost-restore-script/px-restore-snapshot.yaml  --from-file=../argo-worflow/wordpress-app/px-snaphost-restore-script/restore-snapshot.sh -n argo
+sleep 2
+
+cat $ClusterKubeConfigFilePath > config
+echo "Creating kubernetes secret for application git repo cluster kubeconfig. "
+kubectl create secret generic kubernetes-kube-config --from-file=config -n argo
+sleep 2
+export KUBECONFIG=$ClusterKubeConfigFilePath
+echo "                                                    "
+mkdir -p $gitRepoNamespace
+kubectl get secret git-ssh-key -n $gitRepoNamespace -o jsonpath='{.data.id_rsa}' > ./$gitRepoNamespace/id_rsa.tmp
+sleep 2
+base64 -d ./$gitRepoNamespace/id_rsa.tmp > ./$gitRepoNamespace/id_rsa
+export KUBECONFIG=$CurrentClusterPath
+echo $KUBECONFIG
+echo "Creating kubernetes secret for application code git server ssh private key"
+kubectl create secret generic wordpress-repo-sshkey --from-file=./$gitRepoNamespace/id_rsa -n argo
+sleep 3
+rm -rf $gitRepoNamespace
+
+argo cluster-template create ../argo-worflow/wordpress-app/workflow-templates/clusterworkflowtemplate-for-master.yaml
+argo cluster-template create ../argo-worflow/wordpress-app/workflow-templates/clusterworkflowtemplate-for-dev.yaml
+rm config
