6f315a7a4da9ca68a2489d75218e300f33e815d5
nishit93-hub <64061293+nishit93-hub@users.noreply.github.com>
2022-03-31 12:54:05 +0530

    Update submit-wordpress-workflow-template.sh

--- ci-cd-workflow/scripts/submit-wordpress-workflow-template.sh
+++ ci-cd-workflow/scripts/submit-wordpress-workflow-template.sh
@@ -11,7 +11,6 @@ cp ../argo-worflow/wordpress-app/px-snaphshot-restore-script-template/px-snapsho
 
 
 CurrentClusterPath="$(echo $KUBECONFIG)"
-#echo $CurrentClusterPath
 echo "Creating kubernetes secret for jfrog credentials in argo namespace"
 kubectl create secret generic jfrog-config -n argo \
     --from-file=.dockerconfigjson=../config.json \
@@ -45,7 +44,7 @@ echo "                                                    "
 echo "Creating kubernetes secret for px restore snapshot script "
 kubectl create secret generic wordpress-restore-snapshot-script  --from-file=../argo-worflow/wordpress-app/px-snaphost-restore-script/px-restore-snapshot.yaml  --from-file=../argo-worflow/wordpress-app/px-snaphost-restore-script/restore-snapshot.sh -n argo
 sleep 2
-
+echo "                                           "
 cat $ClusterKubeConfigFilePath > config
 echo "Creating kubernetes secret for application git repo cluster kubeconfig. "
 kubectl create secret generic kubernetes-kube-config --from-file=config -n argo
@@ -53,16 +52,16 @@ sleep 2
 #export KUBECONFIG=$ClusterKubeConfigFilePath
 echo "                                                    "
 mkdir -p $gitRepoNamespace
-kubectl get secret git-ssh-key -n $gitRepoNamespace -o jsonpath='{.data.id_rsa}' > ./$gitRepoNamespace/id_rsa.tmp
+kubectl --kubeconfig=$ClusterKubeConfigFilePath get secret git-ssh-key -n $gitRepoNamespace -o jsonpath='{.data.id_rsa}' > ./$gitRepoNamespace/id_rsa.tmp
 sleep 2
 base64 -d ./$gitRepoNamespace/id_rsa.tmp > ./$gitRepoNamespace/id_rsa
 export KUBECONFIG=$CurrentClusterPath
 #echo $KUBECONFIG
 echo "Creating kubernetes secret for application code git server ssh private key"
-kubectl create secret generic wordpress-repo-sshkey --from-file=./$gitRepoNamespace/id_rsa -n argo
+kubectl --kubeconfig=$ClusterKubeConfigFilePath create secret generic wordpress-repo-sshkey --from-file=./$gitRepoNamespace/id_rsa -n argo
 sleep 3
 rm -rf $gitRepoNamespace
 
-argo cluster-template create ../argo-worflow/wordpress-app/workflow-templates/clusterworkflowtemplate-for-master.yaml
-argo cluster-template create ../argo-worflow/wordpress-app/workflow-templates/clusterworkflowtemplate-for-dev.yaml
+argo --kubeconfig=$ClusterKubeConfigFilePath cluster-template create ../argo-worflow/wordpress-app/workflow-templates/clusterworkflowtemplate-for-master.yaml
+argo --kubeconfig=$ClusterKubeConfigFilePath cluster-template create ../argo-worflow/wordpress-app/workflow-templates/clusterworkflowtemplate-for-dev.yaml
 rm config
