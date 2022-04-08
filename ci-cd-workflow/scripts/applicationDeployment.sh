d37c4467c6aae222341e1d288f2ad58b435c36a9
root <root@sn1-r720-g09-31.puretec.purestorage.com>
2022-04-08 01:16:33 -0700

    updated script

--- ci-cd-workflow/scripts/applicationDeployment.sh
+++ ci-cd-workflow/scripts/applicationDeployment.sh
@@ -1,18 +1,28 @@
+#Creating empty files
 echo > ../argocd/manifests/application-deployment.yaml
 echo > ../argocd/manifests/application-repo-secret.yaml
+
+#Copying temlplate to main manifests file
 cp  ../argocd/manifests-template/application-deployment-template.yaml  ../argocd/manifests/application-deployment.yaml
 cp ../argocd/manifests-template/application-repo-secret-template.yaml  ../argocd/manifests/application-repo-secret.yaml
 
-CurrentClusterPath="$(echo $KUBECONFIG)"
+vCONFIGFILE=../setup-vars/setup-vars
 
-echo "Enter the application  name"
-read appName
-sed -ie "s,XX-appName-XX,$appName,g" ../argocd/manifests/application-deployment.yaml
-echo "                                         "
-echo "Enter the application image name"
-read imagename
-sed -ie "s,XX-imagename-XX,$imagename,g" ../argocd/manifests/application-deployment.yaml
-
+PS3=" Select the application you want to deploy: "
+select gitoption in java-app wordpress-app; do
+  case $gitoption in 
+  java-app)
+  echo "java"
+  break
+  ;;
+  wordpress-app)
+  sed -ie "s,XX-appName-XX,wordpress-app,g" ../argocd/manifests/application-deployment.yaml
+  source ${vCONFIGFILE}
+  sed -ie "s,XX-imagename-XX,$Wordpress_Docker_Image_Main_Branch,g" ../argocd/manifests/application-deployment.yaml
+  break
+  ;;
+  esac
+done  
 number="$(echo $RANDOM)"
 sed -ie "s,XX-number-XX,$number,g" ../argocd/manifests/application-repo-secret.yaml
 sed -ie "s,XX-number-XX,$number,g" ../argocd/manifests/application-deployment.yaml
@@ -28,27 +38,30 @@ read namespace
 echo "                                         "
 
 kubectl create ns $namespace
-
+kubectl label ns $namespace controller=ci-cd
 kubectl create secret generic regcred -n $namespace \
     --from-file=.dockerconfigjson=../config.json \
     --type=kubernetes.io/dockerconfigjson
-    
 echo "                                               " 
 sed -ie "s,XX-namespace-XX,$namespace,g" ../argocd/manifests/application-deployment.yaml
-source ../setup-vars/setup-vars
+
+##Setting kube-configs.
+source ${vCONFIGFILE}
+PodName="$(kubectl --kubeconfig=$PX_Application_MainBranch_KUBECONF_PATH get all -n $PX_Application_MainBranch_Namespace | awk 'FNR == 2 {print$1}' | cut -d"/" -f2)"
+EXTERNALIP="$(kubectl --kubeconfig=$PX_Application_MainBranch_KUBECONF_PATH get all -n $PX_Application_MainBranch_Namespace | grep  -A1 "EXTERNAL-IP" | awk 'FNR == 2 {print$4}')"
+REPONAME="$(kubectl --kubeconfig=$PX_Application_MainBranch_KUBECONF_PATH describe pods $PodName -n $PX_Application_MainBranch_Namespace | grep -A1 'Mounts:' | awk 'FNR == 2 {print}' | cut -d"/" -f5 | awk '{print $1}')"
+echo $EXTERNALIP
+echo $REPONAME
+gitRepoUrl="ssh://git@$EXTERNALIP/home/git/repos/$REPONAME"
 sed -ie "s,XX-url-XX,$gitRepoUrl,g" ../argocd/manifests/application-deployment.yaml
-sed -ie "s,XX-path-XX,$directoryPath,g" ../argocd/manifests/application-deployment.yaml
+sed -ie "s,XX-path-XX,manifest/overlays/development,g" ../argocd/manifests/application-deployment.yaml
 
 encodedurl="$(echo -n $gitRepoUrl |  base64 )"
-#echo $encodedurl
 sed -ie "s,XX-app-code-url-XX,$encodedurl,g" ../argocd/manifests/application-repo-secret.yaml
-#echo $ClusterKubeConfigFilePath
-export KUBECONFIG=$ClusterKubeConfigFilePath
 
-encodedId_rsaKey="$(kubectl get secret git-ssh-key -n $gitRepoNamespace  -o jsonpath='{.data.id_rsa}')"
+encodedId_rsaKey="$(kubectl --kubeconfig=$PX_Application_MainBranch_KUBECONF_PATH get secret git-ssh-key -n $PX_Application_MainBranch_Namespace  -o jsonpath='{.data.id_rsa}')"
 sed -ie "s,XX-sshPrivatekey-XX,$encodedId_rsaKey,g" ../argocd/manifests/application-repo-secret.yaml
 echo "                                            "
-export KUBECONFIG=$CurrentClusterPath
 echo "adding jfrog docker registry details to argocd"
 kubectl apply -f ../argocd/manifests/jfrog-credentials.yaml
 kubectl apply -f ../argocd/manifests/jfrog-registry-configmap.yaml
--- ci-cd-workflow/scripts/installArgoWorkflow.sh
+++
@@ -1,5 +0,0 @@
-kubectl create ns argo &&
-Sleep 5
-kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/quick-start-postgres.yaml &&
-Sleep 5
-kubectl patch svc argo-server -n argo -p '{"spec": {"type": "LoadBalancer"}}'
