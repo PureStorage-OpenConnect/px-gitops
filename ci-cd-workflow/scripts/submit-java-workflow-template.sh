5dc342173ef8a80daddcbe80d423d857f28dcfc7
nishit93-hub <64061293+nishit93-hub@users.noreply.github.com>
2022-04-11 01:04:23 +0530

    Update submit-java-workflow-template.sh

--- ci-cd-workflow/scripts/submit-java-workflow-template.sh
+++ ci-cd-workflow/scripts/submit-java-workflow-template.sh
@@ -1,36 +1,124 @@
-CurrentClusterPath="$(echo $KUBECONFIG)"
-echo $CurrentClusterPath
+echo > ../argo-worflow/java-app/px-snaphost-restore-script/px-restore-snapshot.yaml
+echo > ../argo-worflow/java-app/px-snaphost-restore-script/create-snapshot.sh
+echo > ../argo-worflow/java-app/px-snaphost-restore-script/restore-snapshot.sh
+echo > ../argo-worflow/java-app/px-snaphost-restore-script/px-snapshot.yaml
+echo > ../argo-worflow/java-app/workflow-manifests/clusterworkflowtemplate-for-dev.yaml
+echo > ../argo-worflow/java-app/workflow-manifests/clusterworkflowtemplate-for-master.yaml
+
+cp ../argo-worflow/java-app/px-snaphshot-restore-script-template/px-restore-snapshot.yaml ../argo-worflow/java-app/px-snaphost-restore-script/px-restore-snapshot.yaml
+cp ../argo-worflow/java-app/px-snaphshot-restore-script-template/create-snapshot.sh   ../argo-worflow/java-app/px-snaphost-restore-script/create-snapshot.sh
+cp ../argo-worflow/java-app/px-snaphshot-restore-script-template/restore-snapshot.sh  ../argo-worflow/java-app/px-snaphost-restore-script/restore-snapshot.sh
+cp ../argo-worflow/java-app/px-snaphshot-restore-script-template/px-snapshot.yaml  ../argo-worflow/java-app/px-snaphost-restore-script/px-snapshot.yaml
+cp ../argo-worflow/java-app/workflow-templates/clusterworkflowtemplate-for-dev.yaml   ../argo-worflow/java-app/workflow-manifests/clusterworkflowtemplate-for-dev.yaml
+cp ../argo-worflow/java-app/workflow-templates/clusterworkflowtemplate-for-master.yaml   ../argo-worflow/java-app/workflow-manifests/clusterworkflowtemplate-for-master.yaml
+
+
+vCONFIGFILE=../setup-vars/setup-vars
+if (kubectl get secret -n argo | grep jfrog-config ) 2>&1 >/dev/null
+then
+echo "                                            "
+else
 echo "Creating kubernetes secret for jfrog credentials in argo namespace"
-echo "                                                    "
 kubectl create secret generic jfrog-config -n argo \
-    --from-file=.dockerconfigjson=config.json \
+    --from-file=.dockerconfigjson=../config.json \
     --type=kubernetes.io/dockerconfigjson
-
+fi    
+sleep 2
+echo "                                                    "           
+#Fetching main repositoey details
+source ${vCONFIGFILE}
+PodName="$(kubectl --kubeconfig=$PX_Application_MainBranch_KUBECONF_PATH get all -n $PX_Application_MainBranch_Namespace | awk 'FNR == 2 {print$1}' | cut -d"/" -f2)"
+REPONAME="$(kubectl --kubeconfig=$PX_Application_MainBranch_KUBECONF_PATH describe pods $PodName -n $PX_Application_MainBranch_Namespace | grep -A1 'Mounts:' | awk 'FNR == 2 {print}' | cut -d"/" -f5 | awk '{print $1}')"
+MasterEXTERNALIP="$(kubectl --kubeconfig=$PX_Application_MainBranch_KUBECONF_PATH get all -n $PX_Application_MainBranch_Namespace | grep  -A1 "EXTERNAL-IP" | awk 'FNR == 2 {print$4}')"
 echo "                                                    "
+sed -ie "s,XX-namespace-XX,$PX_Application_MainBranch_Namespace,g" ../argo-worflow/java-app/px-snaphost-restore-script/px-restore-snapshot.yaml
+sed -ie "s,XX-repo-XX,$REPONAME,g" ../argo-worflow/java-app/px-snaphost-restore-script/px-snapshot.yaml
+sed -ie "s,XX-namespace-XX,$PX_Application_MainBranch_Namespace,g"  ../argo-worflow/java-app/px-snaphost-restore-script/create-snapshot.sh
+sed -ie "s,XX-namespace-XX,$PX_Application_MainBranch_Namespace,g" ../argo-worflow/java-app/px-snaphost-restore-script/restore-snapshot.sh
+sed -ie "s,XX-namespace-XX,$PX_Application_MainBranch_Namespace,g" ../argo-worflow/java-app/px-snaphost-restore-script/px-snapshot.yaml
+sleep 2
 
-echo "Creating kubernetes secret for px snapshot script "
-kubectl create secret generic snapshot-script --from-file=./px-snaphost-restore-script/px-snapshot.yaml  --from-file=./px-snaphost-restore-script/create-snapshot.sh -n argo
 
-echo "Creating kubernetes secret for px restore snapshot script "
-kubectl create secret generic restore-snapshot-script  --from-file=./px-snaphost-restore-script/px-restore-snapshot.yaml  --from-file=./px-snaphost-restore-script/restore-snapshot.sh -n argo
 
-source ../../setup-vars/setup-vars
+echo "Creating kubernetes px snapshot script secret for main branch  "
+kubectl create secret generic java-snapshot-script-main-branch --from-file=../argo-worflow/java-app/px-snaphost-restore-script/px-snapshot.yaml  --from-file=../argo-worflow/java-app/px-snaphost-restore-script/create-snapshot.sh -n argo
+sleep 2
 echo "                                                    "
-cat $ClusterKubeConfigFilePathAppCode > config
-echo "Creating kubernetes secret for application git repo cluster kubeconfig in argo namespace "
-kubectl create secret generic kubernetes-kube-config --from-file=config -n argo
-export KUBECONFIG=$ClusterKubeConfigFilePathAppCode
 echo "                                                    "
-mkdir $gitRepoNamespace
-kubectl get secret git-ssh-key -n $gitRepoNamespace -o jsonpath='{.data.id_rsa}' > ./$gitRepoNamespace/id_rsa.tmp
-base64 -d ./$gitRepoNamespace/id_rsa.tmp > ./$gitRepoNamespace/id_rsa
-export KUBECONFIG=$CurrentClusterPath
-echo $KUBECONFIG
+echo "Creating kubernetes px restore snapshot script secret for main branch "
+kubectl create secret generic java-restore-snapshot-script-main-branch  --from-file=../argo-worflow/java-app/px-snaphost-restore-script/px-restore-snapshot.yaml  --from-file=../argo-worflow/java-app/px-snaphost-restore-script/restore-snapshot.sh -n argo
+sleep 2
+echo "                                           "
+cat $PX_Application_MainBranch_KUBECONF_PATH > config
+echo "Creating kubernetes secret for application git repo cluster kubeconfig. "
+kubectl create secret generic java-master-branch-cluster-kube-config --from-file=config -n argo
+sleep 2
+echo "                                                    "
+mkdir -p $PX_Application_MainBranch_Namespace
+kubectl --kubeconfig=$PX_Application_MainBranch_KUBECONF_PATH get secret git-ssh-key -n $PX_Application_MainBranch_Namespace -o jsonpath='{.data.id_rsa}' > ./$PX_Application_MainBranch_Namespace/id_rsa.tmp
+sleep 2
+base64 -d ./$PX_Application_MainBranch_Namespace/id_rsa.tmp > ./$PX_Application_MainBranch_Namespace/id_rsa
 echo "Creating kubernetes secret for application code git server ssh private key"
-kubectl create secret generic repo-sshkey --from-file=./$gitRepoNamespace/id_rsa -n argo
+kubectl  create secret generic java-repo-sshkey --from-file=./$PX_Application_MainBranch_Namespace/id_rsa -n argo
 sleep 3
-rm -rf $gitRepoNamespace
+rm -rf $PX_Application_MainBranch_Namespace
+rm -rf config
+#Fetching Dev repository details
+source ${vCONFIGFILE}
+DevRepoPodName="$(kubectl --kubeconfig=$PX_Application_DevBranch_KUBECONF_PATH get all -n $PX_Application_DevBranch_Namespace | awk 'FNR == 2 {print$1}' | cut -d"/" -f2)"
+REPONAME="$(kubectl --kubeconfig=$PX_Application_DevBranch_KUBECONF_PATH describe pods $DevRepoPodName -n $PX_Application_DevBranch_Namespace | grep -A1 'Mounts:' | awk 'FNR == 2 {print}' | cut -d"/" -f5 | awk '{print $1}')"
+DevBranchName="$(kubectl --kubeconfig=$PX_Application_DevBranch_KUBECONF_PATH exec $DevRepoPodName -n $PX_Application_DevBranch_Namespace -- /bin/bash -c "cd /home/git/repos/$REPONAME && git branch | awk 'FNR == 1 {print$1}' | cut -d '*' -f2")"
+DevEXTERNALIP="$(kubectl --kubeconfig=$PX_Application_DevBranch_KUBECONF_PATH get all -n $PX_Application_DevBranch_Namespace | grep  -A1 "EXTERNAL-IP" | awk 'FNR == 2 {print$4}')"
 
-argo cluster-template create ./workflow-templates/clusterworkflowtemplate-for-master.yaml
-argo cluster-template create ./workflow-templates/clusterworkflowtemplate-for-dev.yaml
+echo > ../argo-worflow/java-app/px-snaphost-restore-script/px-restore-snapshot.yaml
+echo > ../argo-worflow/java-app/px-snaphost-restore-script/create-snapshot.sh
+echo > ../argo-worflow/java-app/px-snaphost-restore-script/restore-snapshot.sh
+echo > ../argo-worflow/java-app/px-snaphost-restore-script/px-snapshot.yaml
+cp ../argo-worflow/java-app/px-snaphshot-restore-script-template/px-restore-snapshot.yaml ../argo-worflow/java-app/px-snaphost-restore-script/px-restore-snapshot.yaml
+cp ../argo-worflow/java-app/px-snaphshot-restore-script-template/create-snapshot.sh   ../argo-worflow/java-app/px-snaphost-restore-script/create-snapshot.sh
+cp ../argo-worflow/java-app/px-snaphshot-restore-script-template/restore-snapshot.sh  ../argo-worflow/java-app/px-snaphost-restore-script/restore-snapshot.sh
+cp ../argo-worflow/java-app/px-snaphshot-restore-script-template/px-snapshot.yaml  ../argo-worflow/java-app/px-snaphost-restore-script/px-snapshot.yaml
+cat $PX_Application_DevBranch_KUBECONF_PATH > config
+sleep 5
+sed -ie "s,XX-namespace-XX,$PX_Application_DevBranch_Namespace,g" ../argo-worflow/java-app/px-snaphost-restore-script/px-restore-snapshot.yaml
+sed -ie "s,XX-repo-XX,$REPONAME,g" ../argo-worflow/java-app/px-snaphost-restore-script/px-snapshot.yaml
+sed -ie "s,XX-namespace-XX,$PX_Application_DevBranch_Namespace,g"  ../argo-worflow/java-app/px-snaphost-restore-script/create-snapshot.sh
+sed -ie "s,XX-namespace-XX,$PX_Application_DevBranch_Namespace,g" ../argo-worflow/java-app/px-snaphost-restore-script/restore-snapshot.sh
+sed -ie "s,XX-namespace-XX,$PX_Application_DevBranch_Namespace,g" ../argo-worflow/java-app/px-snaphost-restore-script/px-snapshot.yaml
+
+sed -ie "s,XX-java_Docker_Image-XX,$java_Docker_Image_Dev_Branch,g" ../argo-worflow/java-app/workflow-manifests/clusterworkflowtemplate-for-dev.yaml
+sed -ie "s,XX-java_Docker_Image-XX,$java_Docker_Image_Main_Branch,g" ../argo-worflow/java-app/workflow-manifests/clusterworkflowtemplate-for-master.yaml
+sed -ie "s,XX-PX_Application_DevBranch-XX,$DevBranchName,g" ../argo-worflow/java-app/workflow-manifests/clusterworkflowtemplate-for-dev.yaml
+sed -ie "s,XX-EXTERNALIP-XX,$DevEXTERNALIP,g" ../argo-worflow/java-app/workflow-manifests/clusterworkflowtemplate-for-dev.yaml
+sed -ie "s,XX-EXTERNALIP-XX,$MasterEXTERNALIP,g" ../argo-worflow/java-app/workflow-manifests/clusterworkflowtemplate-for-master.yaml
+sed -ie "s,XX-RepoName-XX,$REPONAME,g" ../argo-worflow/java-app/workflow-manifests/clusterworkflowtemplate-for-dev.yaml
+sed -ie "s,XX-RepoName-XX,$REPONAME,g" ../argo-worflow/java-app/workflow-manifests/clusterworkflowtemplate-for-master.yaml
+sleep 4
+echo "Creating kubernetes px snapshot script secret for dev branch  "
+kubectl create secret generic java-snapshot-script-dev-branch --from-file=../argo-worflow/java-app/px-snaphost-restore-script/px-snapshot.yaml  --from-file=../argo-worflow/java-app/px-snaphost-restore-script/create-snapshot.sh -n argo
+sleep 2
+echo "                                                    "
+echo "                                                    "
+echo "Creating kubernetes px restore snapshot script secret for dev branch "
+kubectl create secret generic java-restore-snapshot-script-dev-branch  --from-file=../argo-worflow/java-app/px-snaphost-restore-script/px-restore-snapshot.yaml  --from-file=../argo-worflow/java-app/px-snaphost-restore-script/restore-snapshot.sh -n argo
+sleep 2
+echo "                                           "
+kubectl create secret generic java-dev-branch-cluster-kube-config --from-file=config -n argo
+
+    countYaml=`ls -1 ../argo-worflow/java-app/px-snaphost-restore-script/*.yamle 2>/dev/null | wc -l`
+    countScripts=`ls -1 ../argo-worflow/java-app/px-snaphost-restore-script/*.she 2>/dev/null | wc -l`
+    if [ "$countYaml" != "0" ] && [ "$countScripts" != "0" ]
+    then 
+    rm ../argo-worflow/java-app/px-snaphost-restore-script/*.yamle
+    rm ../argo-worflow/java-app/px-snaphost-restore-script/*.she
+    fi
+
+    countYaml=`ls -1 ../argo-worflow/java-app/workflow-manifests/*.yamle 2>/dev/null | wc -l`
+    if [ $countYaml != "0" ]
+    then 
+    rm ../argo-worflow/java-app/workflow-manifests/*.yamle
+    fi
+    sleep 2
+argo  cluster-template create ../argo-worflow/java-app/workflow-manifests/clusterworkflowtemplate-for-master.yaml
+argo  cluster-template create ../argo-worflow/java-app/workflow-manifests/clusterworkflowtemplate-for-dev.yaml
 rm config
